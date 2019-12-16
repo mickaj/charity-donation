a5d1b3e8b7ac187f398d432adf6cd69a9eef165f
Mic Kajzer <mickaj@mkajzer.pl>
2019-12-16 19:59:26 +0100

    DonationController added

---
+++ WebUI/Controllers/DonationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    [Authorize]
+    public class DonationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Donate()
+        {
+            return Content("donate form - get");
+        }
+
+        [HttpPost]
+        public IActionResult Donate(string x)
+        {
+            return Content("donate form- post");
+        }
+    }
+}
