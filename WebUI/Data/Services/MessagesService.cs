8b062d1a32b467cf1c2768e508e2b9a78647b089
Mic Kajzer <mickaj@mkajzer.pl>
2020-01-06 13:15:55 +0100

    messages service re-add

---
+++ WebUI/Data/Services/MessagesService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Data.DataModel;
+using WebUI.Data.Services.Interfaces;
+
+namespace WebUI.Data.Services
+{
+    public class MessagesService : ServiceBase, IMessagesService
+    {
+        public MessagesService(CharityDbContext context)
+            : base(context)
+        {
+        }
+
+        public void AddMessage(string name, string lastName, string message)
+        {
+            _context.Messages.Add(new Message() { Name = name, LastName = lastName, MessageBody = message });
+            _context.SaveChanges();
+        }
+    }
+}
